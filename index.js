629479f8f21d7b757c1480411e1874729cfa2af1
Jochem Dijkdrent <dijk0146@hz.nl>
2023-01-22 21:13:50 +0100

    changed the response

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('You have arrived on the NodeApp Site! Welcome!')
 })
 
 app.listen(port, () => {
